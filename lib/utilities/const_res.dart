93f72929b4ef2dc15750e173cdef62cfe19a2976
king-hub259 <84419911+king-hub259@users.noreply.github.com>
2026-04-20 23:55:35 +0100

    Change notification topic from 'shortzz' to 'Palaver'

--- lib/utilities/const_res.dart
+++ lib/utilities/const_res.dart
@@ -3,7 +3,7 @@ const String apiURL = '${baseURL}api/';
 const String apiKey = 'retry123';
 
 // If you change this topic you also change backend .env file
-String notificationTopic = "shortzz";
+String notificationTopic = "Palaver";
 
 String revenueCatAndroidApiKey = "______"; // revenueCat android api
 String revenueCatAppleApiKey = "________"; // revenueCat apple api
