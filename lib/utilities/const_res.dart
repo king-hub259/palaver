8df18e57a1234e56eeab18176c436f5f3560ce77
king-hub259 <michealige080@gmail.com>
2026-04-20 14:18:57 -0700

    main

---
+++ lib/utilities/const_res.dart
@@ -0,0 +1,9 @@
+const String baseURL = 'https://chat.edubest.com.ng/';
+const String apiURL = '${baseURL}api/';
+const String apiKey = 'retry123';
+
+// If you change this topic you also change backend .env file
+String notificationTopic = "shortzz";
+
+String revenueCatAndroidApiKey = "______"; // revenueCat android api
+String revenueCatAppleApiKey = "________"; // revenueCat apple api
